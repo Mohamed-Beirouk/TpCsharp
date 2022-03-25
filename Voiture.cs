dde91d8a4e8db317f59c03a0436c38ef7b5068f9
Mohamed Beirouk <mbeirouck@gmail.com>
2022-03-25 14:37:21 +0000

    fc

---
+++ Voiture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPoop
+{
+    public class Voiture
+    {
+        private string modele;
+        private string couleur;
+        private int prix;
+        private int puissance;
+        public Voiture()
+        {
+        }
+        public Voiture(string modele)
+        {
+            this.modele = modele;
+        }
+        public string GetCouleur()
+        {
+            return this.couleur;
+        }
+        public int GetPrix()
+        {
+            return this.prix;
+        }
+
+
+
+
+
+
+    }
+}
